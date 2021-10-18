b7c34a4715bdae45acddeebfad6db339810c0025
Alejandro Juárez <alejandrojuarezck@gmail.com>
2021-10-18 15:30:12 -0600

    Update index.js

--- RCTNTVINVGASAPP/index.js
+++ RCTNTVINVGASAPP/index.js
@@ -1,10 +1,21 @@
 'use strict'
 
+import React from 'react'
 import App from './app/app'
-import { AppRegistry, LogBox } from 'react-native'
 import { name as appName } from './app.json'
+import { AppRegistry, LogBox } from 'react-native'
+import { Provider } from 'react-native-paper'
 
 import 'react-native-gesture-handler'
-
 LogBox.ignoreAllLogs()
-AppRegistry.registerComponent(appName, () => App)
+
+export default function Main () {
+	return (
+		<Provider>
+			<App />
+		</Provider>
+	)
+}
+
+
+AppRegistry.registerComponent(appName, () => Main)
